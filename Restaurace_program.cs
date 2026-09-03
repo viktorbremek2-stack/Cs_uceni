38b04eaedb5d7cba5a1f28bfc8650d60060bc4bc
Viktor Břemek <viktor.bremek2@gmail.com>
2026-09-03 16:39:48 +0200

    Add files via upload

---
+++ Restaurace_program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("Napiš cenu jídla:");
+    double cenaJidla = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Napiš cenu nápoje:");
+    double cenaNapoje = Convert.ToDouble(Console.ReadLine());
+    double celkovaCena = cenaJidla + cenaNapoje;
+    Console.WriteLine("Celková cena je: " + celkovaCena);
+    Console.WriteLine("Chcete rozdělit cenu mezi více lidí? (ano/ne)");
+    string odpoved = Console.ReadLine();
+    if (odpoved.ToLower() == "ano")
+    {
+      Console.WriteLine("Kolik lidí se bude podílet na platbě?");
+      int pocetLidi = Convert.ToInt32(Console.ReadLine());
+      double cenaNaOsobu = celkovaCena / pocetLidi;
+      Console.WriteLine("Cena na osobu je: " + cenaNaOsobu);
+    }
+
+  }
+}
