9b38435d24b8700d4dceb173326e2a57c83cb804
Viktor Břemek <viktor.bremek2@gmail.com>
2026-09-06 12:38:12 +0200

    Add files via upload

---
+++ HadejCislo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    int tajneCislo = 43;
+    int tip;
+
+    do
+    {
+      Console.WriteLine("Tipni si číslo");
+      tip = Convert.ToInt32(Console.ReadLine());
+      if (tip < tajneCislo)
+      {
+        Console.WriteLine("Tvoje číslo je menší než tajné číslo");
+      }
+      else if (tip > tajneCislo)
+      {
+        Console.WriteLine("Tvoje číslo je větší než tajné číslo");
+      }
+      if (tip == tajneCislo)
+      {
+        Console.WriteLine("Uhodl jsi tajné číslo!!!");
+
+      }
+    }
+    while (tip != tajneCislo);
+
+  }
+}
