e094b78e55fd6960a0ebefb343827e8b8c4c6183
Viktor Břemek <viktor.bremek2@gmail.com>
2026-09-08 16:38:20 +0200

    Add files via upload

---
+++ Kostky.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.Common;
+
+class Program
+{
+  static void Main()
+  {
+
+    Random nahoda = new Random();
+
+
+    Console.WriteLine("Napiš 1 aby jsi hodil kostkou");
+    if (Console.ReadLine() != "1")
+      return;
+    int nahodaHrace = nahoda.Next(1, 7);
+    int nahodaPocitace = nahoda.Next(1, 7);
+    if (nahodaHrace > nahodaPocitace)
+    {
+      Console.WriteLine($"Vyhral jsi, pocitac hodil {nahodaPocitace}");
+    }
+    if (nahodaPocitace > nahodaHrace)
+      Console.WriteLine($"Počítač vyhrál, hodil {nahodaPocitace}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
