f218c3c974da324c8000e78b0f3f6153fe118c79
Viktor Břemek <viktor.bremek2@gmail.com>
2026-09-08 15:40:32 +0200

    Add files via upload

---
+++ Kamen.nuzky.papir.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    Random nahoda = new Random();
+    int pocitac;
+
+    Console.WriteLine("Vyber si jedno: kamen/nuzky/papir");
+
+
+    string hrac = Console.ReadLine()!.ToLower().Trim();
+
+    // Generování volby počítače (1 = kámen, 2 = nůžky, 3 = papír)
+    pocitac = nahoda.Next(1, 4);
+
+    if (pocitac == 1)
+      Console.WriteLine("Počítač si vybral: kamen");
+    else if (pocitac == 2)
+      Console.WriteLine("Počítač si vybral: nuzky");
+    else if (pocitac == 3)
+      Console.WriteLine("Počítač si vybral: papir");
+
+
+    if ((hrac == "kamen" && pocitac == 2) ||
+
+        (hrac == "nuzky" && pocitac == 3) ||
+
+        (hrac == "papir" && pocitac == 1))
+    {
+      Console.WriteLine("Vyhrál jsi!");
+    }
+    else if ((hrac == "kamen" && pocitac == 1) ||
+             (hrac == "nuzky" && pocitac == 2) ||
+            (hrac == "papir" && pocitac == 3))
+    {
+      Console.WriteLine("Remíza!");
+    }
+    else
+    {
+
+      Console.WriteLine("Prohrál jsi!");
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
